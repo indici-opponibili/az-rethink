5fa861c50d11545460830e4367e6f838461f3abd
Davide Baldelli <davide.baldelli@nonstudio.org>
2025-02-27 16:00:39 +0100

    added filament user resource, created aggregate progress data view

---
+++ database/migrations/2025_02_27_135458_crate_aggregate_user_progress.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("CREATE VIEW users_contents_progress_count AS (
+            SELECT u.id AS user_id, COUNT(p.id) AS amount FROM users as u
+                LEFT JOIN content_progress as p ON p.user_id = u.id
+            GROUP BY u.id
+        )");
+
+        DB::statement("CREATE VIEW users_courses_progress_count AS (
+            SELECT u.id AS user_id, COUNT(p.id) AS amount FROM users as u
+                LEFT JOIN course_progress as p ON p.user_id = u.id
+            GROUP BY u.id
+        )");
+
+        DB::statement("CREATE VIEW users_achievements_count AS (
+            SELECT u.id AS user_id, COUNT(p.id) AS amount FROM users as u
+                LEFT JOIN achievements as p ON p.user_id = u.id
+            GROUP BY u.id
+        )");
+
+        DB::statement("CREATE VIEW users_glossary_words_count AS (
+            SELECT u.id AS user_id, COUNT(p.id) AS amount FROM users as u
+                LEFT JOIN glossary_word_progress as p ON p.user_id = u.id
+            GROUP BY u.id
+        )");
+
+        DB::statement("CREATE VIEW user_progress_aggregate_data AS (
+            SELECT u.email,
+                u_con.amount AS content_amount,
+                u_cou.amount AS courses_amount,
+                u_w.amount AS words_amount,
+                u_a.amount achievements_amount
+            FROM users as u
+                LEFT JOIN users_contents_progress_count AS u_con ON u.id = u_con.user_id
+                LEFT JOIN users_courses_progress_count AS u_cou ON u.id = u_cou.user_id
+                LEFT JOIN users_achievements_count AS u_a ON u.id = u_a.user_id
+                LEFT JOIN users_glossary_words_count AS u_w ON u.id = u_w.user_id
+        )");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::statement("DROP VIEW IF EXISTS users_contents_progress_count");
+        DB::statement("DROP VIEW IF EXISTS users_courses_progress_count");
+        DB::statement("DROP VIEW IF EXISTS users_achievements_count");
+        DB::statement("DROP VIEW IF EXISTS users_glossary_words_count");
+        DB::statement("DROP VIEW IF EXISTS user_progress_aggregate_data");
+    }
+};
